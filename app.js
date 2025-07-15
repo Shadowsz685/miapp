d9accb9decd76afe463ed1324631a370485f8ce5
Vicente <anibal.vicente.godoy.ramirez@gmail.com>
2025-07-15 13:12:33 -0400

    Seq ID and Delete

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const formulario = document.querySelector(".from-create");
+const formulario = document.querySelector(".form-create");
+let contador = 1;
 formulario.addEventListener("submit", (e)=>{
     e.preventDefault();
     const descripcion = document.querySelector("#descripcion").value;
     const contenedor = document.querySelector(".publicaciones");
-    contenedor.innerHTML +=` <div class="publicacion">
-            <p>id: 00</p>
+    contenedor.innerHTML += `<div class="publicacion">
+            <p>id:${contador}</p>
             <p>${descripcion}</p>
-            <button>Editar</button>
-            <button>Eliminar</button>
-        </div>`
+            <button id="editar" onclick="editar(this)">Editar</button>
+            <button id ="eliminar" onclick="eliminar(this)">Eliminar</button>
+        </div>`; 
+        contador++;
 });
+
+function editar(e) {
+    const publicacion = e.parentElement;
+    const descripcion = publicacion.querySelector("p:nth-child(2)");
+    const nuevoTexto = prompt("Editar descripción:", descripcion.textContent);
+    if (nuevoTexto !== null) {
+        descripcion.textContent = nuevoTexto;
+    }
+}
+
+function eliminar(e) {
+    const publicacion = e.parentElement;
+    publicacion.remove();
+}
